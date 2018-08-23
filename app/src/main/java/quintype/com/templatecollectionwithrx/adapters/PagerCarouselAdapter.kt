ec8d2361344cf049d6c8e02126a4182e2fad23be
Rakshith <rakshith@quintype.com>
2018-08-23 15:39:32 +0530

    Fixed pager carousel issue

--- app/src/main/java/quintype/com/templatecollectionwithrx/adapters/PagerCarouselAdapter.kt
+++ app/src/main/java/quintype/com/templatecollectionwithrx/adapters/PagerCarouselAdapter.kt
@@ -1,14 +1,16 @@
 package quintype.com.templatecollectionwithrx.adapters
 
-import android.support.v4.view.PagerAdapter
-import android.view.ViewGroup
 import android.os.Bundle
 import android.os.Parcelable
+import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.title_inside_image_header_row.view.*
 import quintype.com.templatecollectionwithrx.R
-import quintype.com.templatecollectionwithrx.models.*
+import quintype.com.templatecollectionwithrx.models.AssociatedMetadata
+import quintype.com.templatecollectionwithrx.models.CollectionItem
+import quintype.com.templatecollectionwithrx.models.Story
 import quintype.com.templatecollectionwithrx.ui.main.viewholders.PagerCarouselViewHolder
 import quintype.com.templatecollectionwithrx.ui.main.viewholders.TitleInsideImageViewHolder
 
@@ -23,32 +25,42 @@ class PagerCarouselAdapter(collectionAssociatedMetadata: AssociatedMetadata?, co
     }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        return view === `object`
+        return view == `object`
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-//        var view = LayoutInflater.from(container.context).inflate(R.layout.title_inside_image_header_row, container, false)
-        val slideShowHolder = TitleInsideImageViewHolder.create(container)
+        var view = LayoutInflater.from(container.context).inflate(R.layout.pager_carousel_title_inside_image_row, container, false)
+        var slideShowViewHolder = PagerCarouselViewHolder(view)
         val story = storyList?.get(position)?.story as Story
-        slideShowHolder.bind(story, mCollectionAssociatedMetadata)
-//        container.setOnClickListener(View.OnClickListener {
-//            fragmentCallbacks.addFragment(StoryPagerFragment.create(StoriesList(storyList), position, true),
-//                    StoryDetailFragment::class.java!!.getSimpleName())
-//        })
-        return container
+
+        slideShowViewHolder.bind(story, mCollectionAssociatedMetadata)
+        container.addView(view)
+        return view
     }
 
+//    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+////        var view = LayoutInflater.from(container.context).inflate(R.layout.title_inside_image_header_row, container, false)
+//        val slideShowHolder = TitleInsideImageViewHolder.create(container)
+//        val story = storyList?.get(position)?.story as Story
+//        slideShowHolder.bind(story, mCollectionAssociatedMetadata)
+////        container.setOnClickListener(View.OnClickListener {
+////            fragmentCallbacks.addFragment(StoryPagerFragment.create(StoriesList(storyList), position, true),
+////                    StoryDetailFragment::class.java!!.getSimpleName())
+////        })
+//        return container
+//    }
 
-    override fun saveState(): Parcelable? {
-        val bundle = super.saveState() as Bundle?
-        if (bundle != null) {
-            // Never maintain any states from the base class, just null it out
-            bundle.putParcelableArray("states", null)
-        } else {
-            // do nothing
-        }
-        return bundle
-    }
+
+//    override fun saveState(): Parcelable? {
+//        val bundle = super.saveState() as Bundle?
+//        if (bundle != null) {
+//            // Never maintain any states from the base class, just null it out
+//            bundle.putParcelableArray("states", null)
+//        } else {
+//            // do nothing
+//        }
+//        return bundle
+//    }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as View)
--- app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/PagerCarouselViewHolder.kt
+++ app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/PagerCarouselViewHolder.kt
@@ -8,26 +8,19 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 import quintype.com.templatecollectionwithrx.R
 import quintype.com.templatecollectionwithrx.models.AssociatedMetadata
+import quintype.com.templatecollectionwithrx.models.Image
 import quintype.com.templatecollectionwithrx.models.Story
 
 class PagerCarouselViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-//    init {
-//        var ivHeroImage = itemView?.findViewById<ImageView>(R.id.title_inside_image_header_row_iv_hero_icon)
-//        var tvTitle = itemView?.findViewById<TextView>(R.id.title_inside_image_header_row_tv_title)
-//
-//        tvTitle?.text = collectionItem?.headline
-//
-//        val heroImageURL = "https://" + "images.assettype.com" + "/" + collectionItem.heroImageS3Key
-//
-//        Glide.with(itemView?.context as Context)
-//                .load(heroImageURL)
-//                .into(ivHeroImage as ImageView)
-//    }
+    var ivHeroImage: ImageView? = null
+    var tvTitle: TextView? = null
+
+    init {
+        ivHeroImage = itemView?.findViewById<ImageView>(R.id.pager_carousel_title_inside_image_row_iv_hero_icon)
+        tvTitle = itemView?.findViewById<TextView>(R.id.pager_carousel_title_inside_image_row_tv_title)
+    }
 
     fun bind(collectionItem: Story, collectionAssociatedMetadata: AssociatedMetadata?) {
-        var ivHeroImage = itemView?.findViewById<ImageView>(R.id.title_inside_image_header_row_iv_hero_icon)
-        var tvTitle = itemView?.findViewById<TextView>(R.id.title_inside_image_header_row_tv_title)
-
         tvTitle?.text = collectionItem?.headline
 
         val heroImageURL = "https://" + "images.assettype.com" + "/" + collectionItem.heroImageS3Key
