0b0cf953933b49b9f89a97e0dc982f98b63af2f7
[rakshi059@gmail.com] <[rakshi059@gmail.com]>
2018-10-10 11:31:13 +0530

    Changed imageview to frisco simpleDraweeView

--- app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/RightImageChildViewHolder.kt
+++ app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/RightImageChildViewHolder.kt
@@ -4,8 +4,7 @@ import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import com.facebook.drawee.view.SimpleDraweeView
 import quintype.com.templatecollectionwithrx.R
 import quintype.com.templatecollectionwithrx.models.collection.AssociatedMetadata
 import quintype.com.templatecollectionwithrx.models.story.Story
@@ -14,7 +13,7 @@ class RightImageChildViewHolder(itemView: View?) : BaseTitleBelowImageUnderlineS
     override fun bind(collectionItem: Story, collectionAssociatedMetadata: AssociatedMetadata?, listner: View.OnClickListener) {
         super.bind(collectionItem, collectionAssociatedMetadata, listner)
 //        var tvStoryTitle = itemView.findViewById<TextView>(R.id.right_image_child_row_tv_title)
-        var ivStoryHeroImage = itemView.findViewById<ImageView>(R.id.right_image_child_row_iv_hero_icon)
+        var ivStoryHeroImage = itemView.findViewById<SimpleDraweeView>(R.id.right_image_child_row_iv_hero_icon)
 
         var cvMainContainer = itemView?.findViewById<CardView>(R.id.right_image_child_row_cv_main_container)
         cvMainContainer?.setOnClickListener(listner)
@@ -24,9 +23,11 @@ class RightImageChildViewHolder(itemView: View?) : BaseTitleBelowImageUnderlineS
 
         val heroImageURL = cdnHostName + collectionItem.heroImageS3Key
 
-        Glide.with(ivStoryHeroImage.context)
-                .load(heroImageURL)
-                .into(ivStoryHeroImage)
+        ivStoryHeroImage.setImageURI(heroImageURL)
+
+//        Glide.with(ivStoryHeroImage.context)
+//                .load(heroImageURL)
+//                .into(ivStoryHeroImage)
     }
 
     companion object {
--- app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/TitleInsideImageHorizontalViewHolder.kt
+++ app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/TitleInsideImageHorizontalViewHolder.kt
@@ -1,27 +1,22 @@
 package quintype.com.templatecollectionwithrx.ui.main.viewholders.collectionholders
 
-import android.content.Context
-import android.content.Intent
 import android.support.constraint.ConstraintLayout
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
+import com.facebook.drawee.view.SimpleDraweeView
 import quintype.com.templatecollectionwithrx.R
-import quintype.com.templatecollectionwithrx.ui.main.activities.WebActivity
 import quintype.com.templatecollectionwithrx.models.collection.AssociatedMetadata
 import quintype.com.templatecollectionwithrx.models.story.Story
-import quintype.com.templatecollectionwithrx.utils.Constants
 import quintype.com.templatecollectionwithrx.utils.Utilities
 import quintype.com.templatecollectionwithrx.utils.widgets.CustomRatingBar
 
 class TitleInsideImageHorizontalViewHolder(itemView: View?) : BaseTitleBelowImageBlockSectionViewHolder(itemView) {
     override fun bind(collectionItem: Story, collectionAssociatedMetadata: AssociatedMetadata?, listner: View.OnClickListener) {
         super.bind(collectionItem, collectionAssociatedMetadata, listner)
-        var ivHeroImage = itemView?.findViewById<ImageView>(R.id.title_below_image_block_section_horizontal_scroll_row_iv_hero_icon)
+        var ivHeroImage = itemView?.findViewById<SimpleDraweeView>(R.id.title_below_image_block_section_horizontal_scroll_row_iv_hero_icon)
         var clMainContainer = itemView?.findViewById<ConstraintLayout>(R.id.title_below_image_block_section_horizontal_scroll_row_cl_main_container)
         var rbCustomRatingBar = itemView?.findViewById<CustomRatingBar>(R.id.section_block_title_author_row_item_rating_bar)
         var tvStoryTitle = itemView?.findViewById<TextView>(R.id.section_block_title_author_row_tv_title)
@@ -31,9 +26,11 @@ class TitleInsideImageHorizontalViewHolder(itemView: View?) : BaseTitleBelowImag
         clMainContainer?.setOnClickListener(listner)
         clMainContainer?.tag = adapterPosition
 
-        Glide.with(ivHeroImage?.context as Context)
-                .load(heroImageURL)
-                .into(ivHeroImage)
+        ivHeroImage?.setImageURI(heroImageURL)
+
+//        Glide.with(ivHeroImage?.context as Context)
+//                .load(heroImageURL)
+//                .into(ivHeroImage)
 
 //        ivHeroImage.setOnClickListener(object : View.OnClickListener {
 //            override fun onClick(v: View?) {
