7ac5dc8800a1af48fb135b2243fe595dbc5b9586
Rakshith <rakshi059@gmail.com>
2018-10-30 14:14:21 +0530

    Added shimmer effect

--- app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/CollectionViewHolder.kt
+++ app/src/main/java/quintype/com/templatecollectionwithrx/ui/main/viewholders/collectionholders/CollectionViewHolder.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.android.synthetic.main.fragment_tag_list.*
 import quintype.com.templatecollectionwithrx.R
 import quintype.com.templatecollectionwithrx.adapters.InnerCollectionAdapter
@@ -29,6 +30,7 @@ class CollectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var rvInnerCollection: RecyclerView? = null
     var progressBar: ProgressBar? = null
+    var shimmerView: ShimmerFrameLayout? = null
     var isGridFirstTime = true
 
     fun bind(collectionItem: BulkTableModel) {
@@ -53,7 +55,18 @@ class CollectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         rvInnerCollection = itemView?.findViewById(R.id.default_collection_row_rv_inner_collection)
         progressBar = itemView?.findViewById(R.id.default_collection_row_pb_progress_bar)
-        progressBar?.visibility = View.VISIBLE
+        progressBar?.visibility = View.GONE
+
+        shimmerView = itemView?.findViewById(R.id.shimmer_view_container)
+        shimmerView?.startShimmerAnimation()
+
+
+        /**
+         * stop animation and hide the shimmer once we get the data
+         */
+        //todo stop animation and hide the shimmer once we get the data
+//        shimmerView?.stopShimmerAnimation()
+//        shimmerView?.visibility = View.GONE
 
         val collectionList = ArrayList<CollectionInnerListModel>()
         val collectionInnerList = collectionItem.innerCollectionResponse?.items
